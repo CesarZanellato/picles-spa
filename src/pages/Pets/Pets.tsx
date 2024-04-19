f866de3dfc6c5f3a1cc2e186ac3f0636b69f7664
Cesar Zanellato <cesarlogrc@gmail.com>
2024-04-18 22:25:45 -0300

    criação Pagination, usePetList, atualizações Pets.tsx, index, main

--- src/pages/Pets/Pets.tsx
+++ src/pages/Pets/Pets.tsx
@@ -3,18 +3,49 @@ import { Header } from "../../components/common/Header";
 import { Grid } from "../../components/layout/Grid";
 import styles from "./Pets.module.css";
 import { Skeleton } from "../../components/common/Skeleton";
+import { Pagination } from "../../components/common/Pagination";
+import { useSearchParams } from "react-router-dom";
 
 export function Pets() {
-  
+  const [searcParams, setSearchParams] = useSearchParams();
+
+  const urlParams = {
+    page: searcParams.get('page') ? Number(searcParams.get('page')) : 1,
+  }
+
+
+  function changePage(page: number){
+    setSearchParams((params)=>{
+      params.set('page', String(page))
+      return params
+    })
+  }
+
   return (
     <Grid>
       <div className={styles.container}>
         <Header />
+        {isLoading && (
+          <Skeleton containerClassName={styles.skeleton} count={10} />
+        )}
         <main className={styles.list}>
-          <Skeleton count={5} containerClassName={styles.skeleton} />
-          <Card href='pets/1' text='nina' thumb="" />
-          
+          {/* <Skeleton count={5} containerClassName={styles.skeleton} /> */}
+          {data?.items?.map((pet) => (
+            <Card
+              key={pet.id}
+              href={`/pet/${pet.id}`}
+              text={pet.name}
+              thumb={pet.photo}
+            ></Card>
+          ))}
         </main>
+        {data?.currentPage && (
+          <Pagination
+            currentPage={data.currentPage}
+            totalPages={data.totalPages}
+            onPageChange={(number) => changePage(number)}
+          />
+        )}
       </div>
     </Grid>
   );
